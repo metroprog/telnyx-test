29fbefbc7febca54a656e4523db3f273bb059ec5
metroprog <h-e-l-e-n@i.ua>
2022-09-24 00:13:45 +0300

    2st test

--- cypress/e2e/pages/storage.page.js
+++ cypress/e2e/pages/storage.page.js
@@ -1,24 +1,45 @@
 const joinTheWaitListButtons = '[href*="#form"]';
-const joinForm = '#form form';
-const firstNameInput = '#FirstName';
-const lastNameInput = '#LastName';
-const emailInput = '#Email';
+const joinForm = "#form form";
+const firstNameInput = "#FirstName";
+const lastNameInput = "#LastName";
+const emailInput = "#Email";
 const submitButton = '[type="submit"]';
 const signUpButtons = 'main [href*="sign-up"]';
 const storageButtons = 'main [href*="storage"]';
+const errorMessages = {
+    firstName: "#ValidMsgFirstName",
+    lastName: "#ValidMsgLastName",
+    email: "#ValidMsgEmail",
+};
 
 class StoragePage {
     goToJoinForm() {
         cy.get(joinTheWaitListButtons).first().should("have.text", "Join the waitlist").click();
+        cy.url().should("include", "#form");
+        cy.get(joinForm, { timeout: 10000 }).should("be.visible");
+    }
+
+    fillFirstNameInput(firstName) {
+        cy.get(firstNameInput).clear().type(firstName);
+    }
+
+    fillLastNameInput(lastName) {
+        cy.get(lastNameInput).clear().type(lastName);
+    }
+
+    fillEmailInput(email) {
+        cy.get(emailInput).clear().type(email);
+    }
+
+    submitJoinForm() {
+        cy.get(submitButton).click();
     }
 
     fillAndSubmitJoinForm(userCreds) {
-        cy.url().should("include", "#form");
-        cy.get(joinForm, { timeout: 10000 }).should("be.visible");
-        cy.get(firstNameInput).type(userCreds.firstName);
-        cy.get(lastNameInput).type(userCreds.lastName);
-        cy.get(emailInput).type(userCreds.email);
-        cy.get(submitButton).click();
+        fillFirstNameInput(userCreds.firstName);
+        fillLastNameInput(userCreds.lastName);
+        fillEmailInput(userCreds.email);
+        submitJoinForm();
     }
 
     assertSuccessSentJoinForm() {
@@ -27,7 +48,10 @@ class StoragePage {
         cy.get(signUpButtons).first().should("have.text", "Create a Telnyx account");
         cy.get(storageButtons).first().should("have.text", "Take me back to Telnyx Storage");
     }
+
+    assertErrorMessage(field, text) {
+        cy.get(errorMessages[field]).should("be.visible").should("have.text", text);
+    }
 }
 
 module.exports = new StoragePage();
-
--- cypress/e2e/specs/storage.cy.js
+++ cypress/e2e/specs/storage.cy.js
@@ -1,4 +1,4 @@
-const storagePage = require('../pages/storage.page');
+const storagePage = require("../pages/storage.page");
 const userCreds = {
     firstName: "Test",
     lastName: "User",
@@ -17,9 +17,26 @@ describe("Test Storage", () => {
     });
 
     it("Successfully join the waitlist by sending form with valid values", () => {
-        // cy.visit("https://telnyx.com/products/storage");
         storagePage.goToJoinForm();
         storagePage.fillAndSubmitJoinForm(userCreds);
         storagePage.assertSuccessSentJoinForm();
+    });
+
+    it("Cannot submit Join the waitlist form with empty required fields", () => {
+        storagePage.goToJoinForm();
+        storagePage.fillFirstNameInput(userCreds.firstName);
+        storagePage.fillLastNameInput(userCreds.lastName);
+        storagePage.submitJoinForm();
+        storagePage.assertErrorMessage("email", "Must be valid email. example@yourdomain.com");
+        cy.reload(true);
+        storagePage.fillFirstNameInput(userCreds.firstName);
+        storagePage.fillEmailInput(userCreds.email);
+        storagePage.submitJoinForm();
+        storagePage.assertErrorMessage("lastName", "This field is required.");
+        cy.reload(true);
+        storagePage.fillLastNameInput(userCreds.lastName);
+        storagePage.fillEmailInput(userCreds.email);
+        storagePage.submitJoinForm();
+        storagePage.assertErrorMessage("firstName", "This field is required.");
     });
 });
