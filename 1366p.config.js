73f3e1140b50ee71202b6793a8fbd129229bda09
metroprog <h-e-l-e-n@i.ua>
2022-09-29 17:33:28 +0300

    ci edit

--- 1366p.config.js
+++ 1366p.config.js
@@ -1,14 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        reporter: "mochawesome",
+        reporterOptions: {
+            reportDir: "cypress/report",
+            overwrite: false,
+            html: false,
+            json: true,
+        },
+        video: true,
+        videoUploadOnPasses: true,
+        baseUrl: "https://telnyx.com",
+        viewportHeight: 768,
+        viewportWidth: 1366,
     },
-    video: true,
-    videoUploadOnPasses: true,
-    baseUrl: 'https://telnyx.com',
-    viewportHeight: 768,
-    viewportWidth: 1366,
-  },
 });
